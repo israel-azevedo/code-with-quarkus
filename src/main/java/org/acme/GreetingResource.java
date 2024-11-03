d2b21af2b48589b0db7b4b8dfafb96790cebf9ae
israel-azevedo <80106486+israel-azevedo@users.noreply.github.com>
2024-11-03 20:17:31 -0300

    Update GreetingResource.java

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -4,22 +4,26 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
 
 @Path("/")
 public class GreetingResource {
-
+    
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
+    
     @GET
     @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOG.info("Endpoint /hello foi chamado");
         return "Hello RESTEasy";
     }
-
+    
     @GET
     @Path("/health")
     @Produces(MediaType.TEXT_PLAIN)
     public String health() {
-        return "Healty";
+        LOG.info("Endpoint /health foi chamado");
+        return "Service is up";
     }
 }
-
